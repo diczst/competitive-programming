535bfc150af661fb921b9fdb7adb9376dfca19aa
dickyzuerst <dzuerst@gmail.com>
2024-11-08 22:16:50 +0700

    simple array sum

---
+++ kotlin/src/warmup/02-simple-array-sum.kt.kt
@@ -0,0 +1,20 @@
+package warmup
+
+
+/*
+    example : ar = [1,2,3]. 1+2+3 = 6, so return 6
+*/
+
+fun main(){
+    val arr: Array<Int> = arrayOf(1,2,3,4,5)
+    val arrSum = simpleArraySum(arr)
+    println(arrSum)
+}
+
+fun simpleArraySum(arr: Array<Int>): Int{
+    var sum = 0
+    for (element in arr){
+        sum+=element
+    }
+    return sum
+}
