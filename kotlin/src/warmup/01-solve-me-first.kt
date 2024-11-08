468bf586c470d89fd4ac7ead4990fb176dce6ce7
dickyzuerst <dzuerst@gmail.com>
2024-11-08 22:02:19 +0700

    add solve-me-first

--- kotlin/src/warmup/01-solve-me-first.kt
+++ kotlin/src/warmup/01-solve-me-first.kt
@@ -1,5 +1,20 @@
 package warmup
 
-fun main(){
 
+/*
+
+Complete the function solveMeFirst to compute the sum of two integers.
+a = 7
+b = 3
+return 10
+
+ */
+
+fun main(){
+    val sum = solveMeFirst(2,4)
+    println(sum)
+}
+
+fun solveMeFirst(a: Int, b: Int): Int{
+    return a+b
 }
