18f4b5d94a5a379cb9ff11afa8af4bdad31e2676
dickyzuerst <dzuerst@gmail.com>
2024-11-08 22:46:18 +0700

    a very big sum

---
+++ kotlin/src/warmup/04-a-very-big-sum.kt.kt
@@ -0,0 +1,22 @@
+package warmup
+
+/*
+
+example:
+5
+1000000001 1000000002 1000000003 1000000004 1000000005
+return 5000000015
+*/
+
+fun main(){
+    val bigNums: Array<Long> = arrayOf(1000000001,1000000002,1000000003,1000000004,1000000005)
+    println(aVeryBigSum(bigNums))
+}
+
+fun aVeryBigSum(ar: Array<Long>): Long {
+    var sum: Long = 0
+    for(i in ar.indices){
+        sum+=ar[i]
+    }
+    return sum
+}
